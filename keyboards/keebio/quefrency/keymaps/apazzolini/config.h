c7f3fb63bf3fb820714c31d64683e559a41641ee
Andre Azzolini <andre@azzolini.io>
2026-05-18 22:13:00 -0600

    Remove unnecessary config

--- keyboards/keebio/quefrency/keymaps/apazzolini/config.h
+++ keyboards/keebio/quefrency/keymaps/apazzolini/config.h
@@ -17,10 +17,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* Use I2C or Serial, not both */
-
-// #define USE_SERIAL
-// #define USE_I2C
-
 #define HOLD_ON_OTHER_KEY_PRESS
 #define TAPPING_TERM 125
